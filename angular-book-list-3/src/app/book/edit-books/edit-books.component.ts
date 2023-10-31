101cfb1c8d30a167bdfe6e8aa872e7fc33be7d40
Ellatrix <12546112+ella66gr@users.noreply.github.com>
2023-10-31 00:47:10 +0000

    Updated View Books & Edit Books

--- angular-book-list-3/src/app/book/edit-books/edit-books.component.ts
+++ angular-book-list-3/src/app/book/edit-books/edit-books.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../book.model'; // import the Book interface
+import { BookService } from '../book.service'; // import the BookService
 
 @Component({
   selector: 'app-edit-books',
   templateUrl: './edit-books.component.html',
   styleUrls: ['./edit-books.component.css']
 })
-export class EditBooksComponent {
+  
+export class EditBooksComponent implements OnInit {
+  sampleBook: Book;
+  readingList: Book[] = []; // declare a variable of type Book array
 
+  constructor(private bookService: BookService) {
+    this.sampleBook = { // initialize the property in the constructor
+      id: 1,
+      title: '',
+      author: '',
+      publicationYear: 0,
+      type: 'Non-Fiction', // Cannot be initialised with a null value
+      summary: ''
+    };
+  }
+  
+  ngOnInit(): void { // this is a lifecycle hook. Angular calls ngOnInit() shortly after creating a component. It's a good place to put initialization logic.
+    this.sampleBook = this.bookService.getSampleBook(); // call the getSampleBook() method of the BookService and assign the result to the sampleBook property
+    this.readingList = this.bookService.getBooks(); // the getBooks method was defined in the BookService to return the array of books
+  }
 }
--- angular-book-list-3/src/app/book/view-books/view-books.component.ts
+++ angular-book-list-3/src/app/book/view-books/view-books.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../book.model'; // import the Book interface
+import { BookService } from '../book.service'; // import the BookServic
 
 @Component({
   selector: 'app-view-books',
   templateUrl: './view-books.component.html',
   styleUrls: ['./view-books.component.css']
 })
-export class ViewBooksComponent {
 
+export class ViewBooksComponent implements OnInit {
+  sampleBook: Book;
+  someBooks: Book[] = []; // declare a variable of type Book array
+  
+  constructor(private bookService: BookService) {
+    this.sampleBook = { // initialize the property in the constructor
+      id: 1,
+      title: '',
+      author: '',
+      publicationYear: 0,
+      type: 'Fiction', // Cannot be initialised with a null value
+      summary: ''
+    };
+  }
+  
+  ngOnInit(): void { // this is a lifecycle hook. Angular calls ngOnInit() shortly after creating a component. It's a good place to put initialization logic.
+    this.sampleBook = this.bookService.getSampleBook(); // call the getSampleBook() method of the BookService and assign the result to the sampleBook property
+    this.someBooks = this.bookService.getBooks(); // the getBooks method was defined in the BookService to return the array of books
+  }
 }
